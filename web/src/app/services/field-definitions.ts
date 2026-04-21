a12b68f8928b813e6129a55d1534801e3441729e
FrancoBG79 <fbg2468@gmail.com>
2026-04-21 11:58:43 +0200

    fields get service

--- web/src/app/services/field-definitions.ts
+++ web/src/app/services/field-definitions.ts
@@ -1,6 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { catchError, map, Observable } from 'rxjs';
 
+export interface FieldDefinitions {
+  id: string;
+  description: string;
+  field_type: string;
+  compulsory: boolean;
+  additional_information: string;
+  for_entity: string;
+}
+
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
-export class FieldDefinitions {}
+export class FieldDefinitions {
+  private readonly apiUrl = environment.apiUrl;
+  private readonly httpClient = inject(HttpClient);
+
+  getFieldDefinitions(forEntity: string): Observable<FieldDefinitions[]> {
+    return this.httpClient.get<FieldDefinitions[]>(`${this.apiUrl}/fields`, {
+      params: { forEntity }, observe: 'response'
+    }).pipe(
+      map((response: HttpResponse<FieldDefinitions[]>) => {
+        return response.body ?? [];
+      }),
+      catchError((error: HttpErrorResponse) =>  { throw new Error(error.message); })
+    )
+  }
+}
